48ad569d5d0cacc65d92ab4232e13f8228b14ea6
pedropmedina <pedrodelilia@gmail.com>
2019-06-27 16:40:21 -0400

    Implement Homes

--- src/components/Homes/index.js
+++ src/components/Homes/index.js
@@ -1,7 +1,75 @@
 import React from 'react';
 
-import { Container } from './styled';
+// styles
+import * as Styled from './styled';
 
-const Homes = () => <Container>Homes</Container>;
+// assets and data
+import textdata from '../../assets/text.json';
+import svgicons from '../../assets/sprite.svg';
+import house1 from '../../assets/house-1.jpeg';
+import house2 from '../../assets/house-2.jpeg';
+import house3 from '../../assets/house-3.jpeg';
+import house4 from '../../assets/house-4.jpeg';
+import house5 from '../../assets/house-5.jpeg';
+import house6 from '../../assets/house-6.jpeg';
+
+const homesData = [
+  { src: house1, alt: 'house 1' },
+  { src: house2, alt: 'house 2' },
+  { src: house3, alt: 'house 3' },
+  { src: house4, alt: 'house 4' },
+  { src: house5, alt: 'house 5' },
+  { src: house6, alt: 'house 6' }
+];
+
+const Homes = () => (
+  <Styled.Homes>
+    {homesData.map(({ src, alt }, i) => {
+      const { name, location, rooms, area, price } = textdata.homes[i];
+      return (
+        <Styled.Home key={alt}>
+          <Styled.Image src={src} alt={alt} />
+          <Styled.Like>
+            <use href={`${svgicons}#icon-heart-full`}></use>
+          </Styled.Like>
+          <Styled.Name>{name}</Styled.Name>
+
+          <Styled.Text marginTop>
+            <Styled.Icon>
+              <use href={`${svgicons}#icon-map-pin`}></use>
+            </Styled.Icon>
+            <p>{location}</p>
+          </Styled.Text>
+
+          <Styled.Text marginTop>
+            <Styled.Icon>
+              <use href={`${svgicons}#icon-profile-male`}></use>
+            </Styled.Icon>
+            <p>{rooms}</p>
+          </Styled.Text>
+
+          <Styled.Text>
+            <Styled.Icon>
+              <use href={`${svgicons}#icon-expand`}></use>
+            </Styled.Icon>
+            <p>
+              {area}
+              <sup>2</sup>
+            </p>
+          </Styled.Text>
+
+          <Styled.Text>
+            <Styled.Icon>
+              <use href={`${svgicons}#icon-key`}></use>
+            </Styled.Icon>
+            <p>{price}</p>
+          </Styled.Text>
+
+          <Styled.CardButton>Contact realtor</Styled.CardButton>
+        </Styled.Home>
+      );
+    })}
+  </Styled.Homes>
+);
 
 export default Homes;
--- src/components/Homes/styled.js
+++ src/components/Homes/styled.js
@@ -1,6 +1,74 @@
 import styled from 'styled-components/macro';
 
-export const Container = styled.div`
-  background-color: var(--color-secondary);
+import { Button } from '../../stylesShare';
+
+export const Homes = styled.section`
   grid-column: center-start / center-end;
+
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(25rem, 1fr));
+  gap: 7rem;
+  margin: 15rem 0;
+`;
+
+export const Home = styled.div`
+  background-color: var(--color-grey-light-1);
+
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  row-gap: 3.5rem;
+`;
+
+export const Image = styled.img`
+  width: 100%;
+  grid-column: 1 / -1;
+  grid-row: 1 / 2;
+  z-index: 1;
+`;
+
+export const Icon = styled.svg`
+  fill: var(--color-primary);
+  height: 2rem;
+  width: 2rem;
+  margin-right: 1rem;
+`;
+
+export const Like = styled(Icon)`
+  width: 2.5rem;
+  height: 2.5rem;
+  grid-row: 1 / 2;
+  grid-column: 2 / 3;
+  z-index: 2;
+  justify-self: end;
+  margin: 1rem;
+`;
+
+export const Name = styled.h5`
+  grid-row: 1 / 2;
+  grid-column: 1 / -1;
+  justify-self: center;
+  align-self: end;
+  z-index: 3;
+
+  width: 80%;
+  font-family: var(--font-display);
+  font-size: 1.6rem;
+  text-align: center;
+  padding: 1.25rem;
+  background-color: var(--color-secondary);
+  color: #fff;
+  font-weight: 400;
+  transform: translateY(50%);
+`;
+
+export const Text = styled.p`
+  font-size: 1.5rem;
+  margin-left: 2rem;
+  display: flex;
+  align-items: center;
+  margin-top: ${({ marginTop }) => (marginTop ? '2.5rem' : 'initial')};
+`;
+
+export const CardButton = styled(Button)`
+  grid-column: 1 / -1;
 `;
